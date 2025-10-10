941dd2b8168d48b86b969050bebeafd1756728c6
antwi robert <roberttantwi@outlook.com>
2025-10-10 11:47:47 +0000

    add dashboard navbar

---
+++ src/modules/dashboard/dashboard-command.tsx
@@ -0,0 +1,32 @@
+import {
+  Command,
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command"
+import { Dispatch, SetStateAction } from "react";
+
+interface DashboardCommandProps {
+    open: boolean;
+    setOpen: Dispatch<SetStateAction<boolean>>
+}
+
+const DashboardCommand = ({ open, setOpen }: DashboardCommandProps) => {
+  return (
+    <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Find a meeting or agent" />
+        <CommandList>
+            <CommandItem>
+                Test
+            </CommandItem>
+        </CommandList>
+    </CommandDialog>
+  )
+}
+
+export default DashboardCommand
